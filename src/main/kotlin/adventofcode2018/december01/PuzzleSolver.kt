4144b4017150fbed5ded051e1813fe5eb82253e8
mschijf <mschijf@gmail.com>
2023-05-22 22:04:43 +0200

    day1 part2

--- src/main/kotlin/adventofcode2018/december01/PuzzleSolver.kt
+++ src/main/kotlin/adventofcode2018/december01/PuzzleSolver.kt
@@ -11,6 +11,19 @@ class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
     override fun resultPartOne(): Any {
         return inputLines.map{it.toInt()}.sum()
     }
+
+    override fun resultPartTwo(): Any {
+        val frequencySet = mutableSetOf<Int>()
+        val frequenceChangeList = inputLines.map{it.toInt()}
+        var sum = 0
+        var index = 0
+        while (sum !in frequencySet) {
+            frequencySet += sum
+            sum += frequenceChangeList[index % frequenceChangeList.size]
+            index++
+        }
+        return sum
+    }
 }
 
 

fcb6a3a16887764db0d0f4a0d841bc82f90873a2
mschijf <mschijf@gmail.com>
2023-05-29 15:52:14 +0200

    adding grid-print functionality

--- src/main/kotlin/adventofcode2018/december06/PuzzleSolver.kt
+++ src/main/kotlin/adventofcode2018/december06/PuzzleSolver.kt
@@ -2,9 +2,10 @@ package adventofcode2018.december06
 
 import adventofcode2018.PuzzleSolverAbstract
 import tool.position.Coordinate
+import tool.position.print
 
 fun main() {
-    PuzzleSolver(test=false).showResult()
+    PuzzleSolver(test=true).showResult()
 }
 
 class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
@@ -13,21 +14,24 @@ class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
     private val maxX = coordList.maxOf{ it.x }
     private val maxY = coordList.maxOf{ it.y }
 
-
     override fun resultPartOne(): Any {
         val gridMap = createMapPuzzleOne()
-//        gridMap.print()
+        gridMap.print { it.gridValueToString() }
         val singleFields = gridMap.filter { it.value.size == 1 }.map{it.key to it.value.first()}.toMap()
         val borderFields = singleFields.filter{it.key.isBorder()}.values.toSet()
-        val resultFields = singleFields.values.filter{it !in borderFields}.groupingBy { it }.eachCount()
+        val resultFields = singleFields.values.filterNot{it in borderFields}.groupingBy { it }.eachCount()
         return resultFields.values.max()
     }
 
     override fun resultPartTwo(): Any {
         val maxManhattanDistance = if (test) 32 else 10_000
         val gridMap = createMapPuzzleTwo()
-//        gridMap.print2()
+        gridMap.print { item -> if (item < 32) "#" else "." }
         return gridMap.values.count { it < maxManhattanDistance }
+    }
+
+    private fun List<Coordinate>.gridValueToString(): String {
+        return if (this.size > 1) "." else { ('a'+ coordList.indexOf( this.first() )).toString() }
     }
 
     private fun Coordinate.isBorder() : Boolean {
@@ -36,7 +40,7 @@ class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
 
 
     private fun createMapPuzzleOne(): Map<Coordinate, List<Coordinate>> {
-        return (0..maxX).flatMap { x -> (0..maxY).map { y -> Coordinate(x, y) } }
+        return (0..maxX+1).flatMap { x -> (0..maxY).map { y -> Coordinate(x, y) } }
             .associateWith { coord -> coordList.groupBy { it.manhattanDistance(coord) }.minBy { it.key }.value }
     }
 
@@ -45,21 +49,21 @@ class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
             .associateWith { coord -> coordList.sumOf { it.manhattanDistance(coord) } }
     }
 
-    private fun Map<Coordinate, List<Coordinate>>.print() {
-        val maxX = this.keys.maxOf{ it.x }
-        val maxY = this.keys.maxOf{ it.y }
-        for (y in 0..maxY) {
-            for (x in 0..maxX) {
-                if (this[Coordinate(x,y)]!!.size > 1) {
-                    print (".")
-                } else {
-                    val index = coordList.indexOf( this[Coordinate(x,y)]!!.first() )
-                    print('a'+index)
-                }
-            }
-            println()
-        }
-    }
+//    private fun Map<Coordinate, List<Coordinate>>.print() {
+//        val maxX = this.keys.maxOf{ it.x }
+//        val maxY = this.keys.maxOf{ it.y }
+//        for (y in 0..maxY) {
+//            for (x in 0..maxX) {
+//                if (this[Coordinate(x,y)]!!.size > 1) {
+//                    print (".")
+//                } else {
+//                    val index = coordList.indexOf( this[Coordinate(x,y)]!!.first() )
+//                    print('a'+index)
+//                }
+//            }
+//            println()
+//        }
+//    }
 
     private fun Map<Coordinate, Int>.print2() {
         val maxX = this.keys.maxOf{ it.x }
---
+++ src/main/kotlin/tool/position/Grid.kt
@@ -0,0 +1,18 @@
+package tool.position
+
+fun <T> Map<Coordinate, T>.print(default: String="NULL", itemAsString: (T)->String) {
+    val maxX = this.keys.maxByOrNull { it.x }?.x ?: -1
+    val maxY = this.keys.maxByOrNull { it.y }?.y ?: -1
+
+    (0..maxY).forEach { y ->
+        (0..maxX).forEach { x ->
+            val field = Coordinate(x,y)
+            if (this.contains(field)) {
+                print(itemAsString(this[field]!!))
+            } else {
+                print(default)
+            }
+        }
+        println()
+    }
+}

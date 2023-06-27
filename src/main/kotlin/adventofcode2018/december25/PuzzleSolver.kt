feddf34fecbb204661ef82ed986d7908a0acc2ae
mschijf <mschijf@gmail.com>
2023-06-27 16:22:20 +0200

    day 25 - refactored

--- src/main/kotlin/adventofcode2018/december25/PuzzleSolver.kt
+++ src/main/kotlin/adventofcode2018/december25/PuzzleSolver.kt
@@ -11,23 +11,42 @@ class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
 
     private val pointList = inputLines.map{Point4D.of(it)}
 
-    override fun resultPartOne(): Any {
-        var constellations = emptyList<Set<Point4D>>()
+    // we houden een lijst bij met constellations. Een constellation is een Set van Point4D objcten
+    // we beginnen met een lege lijst
+    // Neem het eerste punt
+    //   we kijken in welke constellatie die zou passen (daarvoor maken we gebruik van de gemaakte functie inConstellation())
+    //   dit levert twee groepen pop: de constellations waar het punt wel in past en een goep met constellaties waar die niet in past
+    //   die eerste groep wordt nu in totaal één constellatie, want daar zorgt het nieuwe punt voor. Het nieuwe punt komt daar natuurlijk bij
+    //   de tweede groep blijft een ongewijzigde groep
+    //   voeg nu het samengestelde deel van de eerste groep weer samen met de tweede groep om weer tot een lijst van constellations te komen
+    // En herhaal dit voor all punten
+    //
+    // note: ik heb dit eerst met een for-lus gedaan en die later stukje bij beetje tot één statement weten te vouwen.
+    //       met dank aan de lambda's: partition (om een lijst in twee lijsten te partitioneren)
+    //                                 run (om een actie te doen op het resultaat van de vorige lambda
+    //                                 fold (om de lijst van punten te 'folden' in de groeiende lijst van constellations
 
-        pointList.forEach { point ->
-            var extended = constellations.filter{it.inConstellation(point)}.flatMap{it+point}.toSet()
-            val notExtended = constellations.filterNot { it.inConstellation(point) }
-            if (extended.isEmpty()) {
-                extended = setOf(point)
-            }
-            constellations = notExtended + listOf(extended)
+    override fun resultPartOne(): Any {
+
+        val constellations = pointList.fold(emptyList<Set<Point4D>>()){ constellationList, point ->
+            constellationList
+                .partition { point.inConstellation(it) }
+                .run {listOf(first.flatten().toSet() + point) + second}
         }
-        println(constellations.map{it.size})
+
+// Misschien meer leesbaar. De variant met de for lus
+//        var constellations = emptyList<Set<Point4D>>()
+//        pointList.forEach { point ->
+//            constellations = constellations
+//                .partition { point.inConstellation(it) }
+//                .run {listOf(first.flatten().toSet() + point) + second}
+//        }
+//        println(constellations.map{it.size})
         return constellations.size
     }
 
-    private fun Set<Point4D>.inConstellation(aPoint:Point4D): Boolean {
-        return this.any{it.manhattanDistance(aPoint) <= 3}
+    private fun Point4D.inConstellation(constellation: Set<Point4D>): Boolean {
+        return constellation.any{it.manhattanDistance(this) <= 3}
     }
 }
 
